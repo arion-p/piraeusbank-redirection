bbd2bd77b2bb796b881be95764950d1b029b4dc8
Panayotis Kouvarakis <kouvarakisp@yahoo.gr>
2025-04-21 19:38:37 +0300

    Fix #1 and #2 re payment cancellation

--- Controller/Payment/Cancel.php
+++ Controller/Payment/Cancel.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Natso\Piraeus\Controller\Payment;
 
 use Magento\Framework\App\CsrfAwareActionInterface;
@@ -10,22 +9,13 @@ use Magento\Sales\Api\OrderRepositoryInterface;
 class Cancel extends \Magento\Framework\App\Action\Action implements CsrfAwareActionInterface
 {
     public $context;
-    protected $orderFactory;
-    protected $checkoutSession;
-    protected $orderRepository;
     protected $logger;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Sales\Model\OrderFactory $orderFactory,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        OrderRepositoryInterface $orderRepository,
         \Psr\Log\LoggerInterface $logger
     ) {
         $this->context   = $context;
-        $this->orderFactory = $orderFactory;
-        $this->checkoutSession = $checkoutSession;
-        $this->orderRepository = $orderRepository;
         $this->logger = $logger;
         parent::__construct($context);
     }
@@ -42,24 +32,6 @@ class Cancel extends \Magento\Framework\App\Action\Action implements CsrfAwareAc
 
     public function execute()
     {
-        try {
-            if ($this->checkoutSession->getLastRealOrderId()) {
-                $order = $this->orderFactory->create();
-                $order->loadByIncrementId($this->checkoutSession->getLastRealOrderId());
-                $order->setState(\Magento\Sales\Model\Order::STATE_CANCELED, true);
-                $order->setStatus(\Magento\Sales\Model\Order::STATE_CANCELED);
-                foreach ($this->_order->getAllItems() as $item) { // Cancel order items
-                    $item->cancel();
-                }
-                $order->addStatusToHistory($order->getStatus(), 'Payment canceled from client after redirect.');
-                $this->orderRepository->save($order);
-                $this->_redirect('checkout/onepage/failure');
-            } else {
-                $this->_redirect('/');
-            }
-        } catch (\Exception $e) {
-            $this->logger->error('Piraeus Bank Payment Cancelled: Exception: ' . $e->getMessage());
-            $this->_redirect('/');
-        }
+        $this->_redirect('winbank/payment/docancel');
     }
 }
---
+++ Controller/Payment/DoCancel.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Natso\Piraeus\Controller\Payment;
+
+use Magento\Framework\App\CsrfAwareActionInterface;
+use Magento\Framework\App\Request\InvalidRequestException;
+use Magento\Framework\App\RequestInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+
+class DoCancel extends \Magento\Framework\App\Action\Action implements CsrfAwareActionInterface
+{
+    public $context;
+    protected $orderFactory;
+    protected $checkoutSession;
+    protected $orderRepository;
+    protected $messageManager;
+    protected $logger;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Sales\Model\OrderFactory $orderFactory,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        OrderRepositoryInterface $orderRepository,
+        \Magento\Framework\Message\ManagerInterface $messageManager,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->context   = $context;
+        $this->orderFactory = $orderFactory;
+        $this->checkoutSession = $checkoutSession;
+        $this->orderRepository = $orderRepository;
+        $this->messageManager = $messageManager;
+        $this->logger = $logger;
+        parent::__construct($context);
+    }
+
+    public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException
+    {
+        return null;
+    }
+   
+    public function validateForCsrf(RequestInterface $request): ?bool
+    {
+        return true;
+    }
+
+    public function execute()
+    {
+        try {
+            if ($this->checkoutSession->getLastRealOrderId()) {
+                $order = $this->orderFactory->create();
+                $order->loadByIncrementId($this->checkoutSession->getLastRealOrderId());
+                $order->cancel();
+                //$order->setState(\Magento\Sales\Model\Order::STATE_CANCELED, true);
+                $order->setStatus(\Magento\Sales\Model\Order::STATE_CANCELED);
+                // foreach ($order->getAllItems() as $item) { // Cancel order items
+                //     $item->cancel();
+                // }
+                $order->addStatusToHistory($order->getStatus(), 'Payment canceled from client after redirect.');
+                $this->orderRepository->save($order);
+                $this->messageManager->addErrorMessage(__('Payment canceled by user.'));
+                $this->checkoutSession->restoreQuote();
+                $this->_redirect('checkout/cart');
+            } else {
+                $this->logger->error('Piraeus Bank Payment Cancel: No order found.');
+                $this->_redirect('/');
+            }
+        } catch (\Exception $e) {
+            $this->logger->error('Piraeus Bank Payment Cancelled: Exception: ' . $e->getMessage());
+            $this->_redirect('/');
+        }
+    }
+}
